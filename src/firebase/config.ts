76d069ed7bd49c49cc6c5b0c63c385fc0af8dd78
Pavlo <marlana08022022@gmail.com>
2026-04-02 21:48:22 +0300

    fix: firebase config and google auth (popup)

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBLV0bTJ7fR9bye2BfD-2kSxMUv-KQ_o_Q",
+  authDomain: "gift-app-f36f2.firebaseapp.com",
+  projectId: "gift-app-f36f2",
+  storageBucket: "gift-app-f36f2.appspot.com",
+  messagingSenderId: "942839081000",
+  appId: "1:942839081000:web:7b14cae9a395b241192d1d",
 };
 
 export const app = initializeApp(firebaseConfig);
